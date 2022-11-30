c0a289ca2354fd5c71c2a0b376a506e669be44b6
Maria Luiza Ramos <mariialuiizaramos@gmail.com>
2022-11-30 15:34:33 -0300

    adicionado testes para decrescente.py

--- decrescente.py
+++ decrescente.py
@@ -1,16 +1,27 @@
-def decrescente():
+import unittest
+
+
+def decrescente(arr):
     # Faça um Programa que leia três números e mostre-os em ordem decrescente.
+    arr.sort(reverse=True)
+    return arr
 
-    decrescente_numeros = []
 
-    for n in range(3):
-        num = int(input('Digite um número: '))
+class DecrescenteTest(unittest.TestCase):
 
-        decrescente_numeros.append(num)
+    def test_decrescente(self):
+        self.assertEquals(decrescente([3, 4, 1, 2, 8, 4]), [8, 4, 4, 3, 2, 1])
 
-    decrescente_numeros.sort(reverse=True)
-    print(decrescente_numeros)
+    def test_decrescente_2(self):
+        self.assertEquals(decrescente([3, 1]), [3, 1])
+
+    def test_decrescente_3(self):
+        self.assertEquals(decrescente([1, -2, 3]), [3, 1, -2])
 
 
 if __name__ == '__main__':
-    decrescente()
+    numeros = []
+    for n in range(3):
+        num = int(input('Digite um número: '))
+        numeros.append(num)
+    print(decrescente(numeros))
