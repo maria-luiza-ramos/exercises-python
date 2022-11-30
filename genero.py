5fe89e3bdf70c5f1190d015a9e9da9413974fcc1
Maria Luiza Ramos <mariialuiizaramos@gmail.com>
2022-11-30 15:28:31 -0300

    testes adicionados para genero.py e msg_2.py

--- genero.py
+++ genero.py
@@ -1,20 +1,38 @@
 # Faça um Programa que verifique se uma letra digitada é "F" ou "M".
 # Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+import unittest
 
-def genero(s):
+
+def genero(s: str) -> str:
     """
-    :param s: espera uma string
-    :return: None
+    :param s: uma string contendo o valor 'F' ou 'M'
+    :return: Retorna uma string para a letra informada, caso não seja um dos valores definidos retorna 'outro'
     """
     s = s.upper()[0]
     if s == 'F':
-        print('Feminino')
-    elif s == 'M':
-        print('Masculino')
-    else:
-        print('Outro')
+        return 'Feminino'
+    if s == 'M':
+        return 'Masculino'
+    return 'Outro'
+
+
+class GeneroTest(unittest.TestCase):
+
+    def test_feminino(self):
+        self.assertEquals(genero("f"), "Feminino", "Deveria ter retornado Feminino")
+
+    def test_feminino_maiusculo(self):
+        self.assertEquals(genero("F"), "Feminino",  "Deveria ter retornado Feminino")
+
+    def test_masculino(self):
+        self.assertEquals(genero("m"), "Masculino",  "Deveria ter retornado Masculino")
+
+    def test_masculino_maiusculo(self):
+        self.assertEquals(genero("M"), "Masculino",  "Deveria ter retornado Masculino")
+    def test_outro(self):
+        self.assertEquals(genero("G"), "Outro",  "Deveria ter retornado Outro")
 
 
 if __name__ == '__main__':
     sexo = str(input('Digite o sexo: '))
-    genero(sexo)
+    print(genero(sexo))
--- msg_2.py
+++ msg_2.py
@@ -1,16 +1,39 @@
-def msg2():
-    # Faça um Programa que pergunte em que turno você estuda.
-    # Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
-    # Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+import unittest
 
-    turnInput = str(input('Qual turno você estuda? ')).upper()[0]
+
+# Faça um Programa que pergunte em que turno você estuda.
+# Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+# Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+
+def msg_by_turn(turnInput):
     turn = {
         "M": "Bom dia",
         "V": "Boa Tarde",
         "N": "Boa noite"
     }
-    print(turn.get(turnInput, 'Valor Inválido!'))
+    #.get recebe uma chave e procura no dict e retorna o valor dela, caso não tenha a chave ela retorna
+    # o valor definido no segundo parametro que tem o valor default None
+    return turn.get(turnInput, 'Valor Inválido!')
+
+
+class MsgTest(unittest.TestCase):
+    def test_bom_dia(self):
+        self.assertEqual(msg_by_turn("M"), "Bom dia")
+
+    def test_boa_tarde(self):
+        self.assertEqual(msg_by_turn("V"), "Boa Tarde")
+
+    def test_boa_noite(self):
+        self.assertEqual(msg_by_turn("N"), "Boa noite")
+
+    def test_invalido(self):
+        self.assertEqual(msg_by_turn(3), 'Valor Inválido!')
+
+    def test_invalido_2(self):
+        self.assertEqual(msg_by_turn("B"), 'Valor Inválido!')
 
 
 if __name__ == '__main__':
-    msg2()
+    turnInput = str(input('Qual turno você estuda? ')).upper()[0]
+    print(msg_by_turn(turnInput))
